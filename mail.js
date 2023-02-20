b47c510824b5ab8c162ee059cc8bbca899467dfa
dhruvshah <202151142@iiitvadodara.ac.in>
2023-02-21 01:22:13 +0530

    first commit

---
+++ mail.js
@@ -0,0 +1,40 @@
+const nodemailer = require("nodemailer")
+const express= require("express")
+const dotenv=require("dotenv")
+const app=express()
+const cors=require("cors")
+app.use(cors())
+app.use(express.json())
+
+dotenv.config()
+const port=process.env.PORT || 3000
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'shahdhruvmayank@gmail.com',
+        pass: process.env.key
+    }
+});
+
+app.post("/",async(req,res)=> {
+    try {
+        let mailOptions = {
+            from: 'shahdhruvmayank@gmail.com',
+            to: 'shahdhruvmayank@gmail.com',
+            subject: "Brochure Downloaded",
+            text: `Name: ${req.body.name} \nPhone Number ${req.body.ph_number} \nOrg ${req.body.org} \nEmail ${req.body.mail}` 
+        }
+
+        let info = await transporter.sendMail(mailOptions)
+        console.log(info.response)
+        res.send( info.response)
+
+    } catch (err) {
+        console.log(err)
+        res.send(err)
+    }
+}) 
+
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`)
+})
